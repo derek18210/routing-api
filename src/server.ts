12b7e989ed29c53adec5532d505c6ab77d14d50a
Derek Lee <derek18210@gmail.com>
2022-02-25 19:00:35 +0800

    init #2

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+
+import  express, { Express, Request, Response } from 'express';
+import { QuoteHandlerInjector } from '.././lib/handlers/quote/injector';
+import { QuoteHandler } from '.././lib/handlers/quote/quote';
+
+const port = 5050;
+
+const app: Express = express();
+
+const quoteInjectorPromise = new QuoteHandlerInjector('quoteInjector').build()
+const quoteHandler = new QuoteHandler('quote', quoteInjectorPromise)
+
+
+app.get('/', (request: Request, response: Response) => {
+  console.log(request.cookies);
+  
+  quoteHandler: quoteHandler.handler;
+
+  response.type('text/plain');
+  response.send('Homepage');
+})
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`)}
+);
