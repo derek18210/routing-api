a5f89cec1cd6390dde8929c1e8ffdb979eeeb7da
Derek Lee <derek18210@gmail.com>
2022-02-28 20:49:04 +0800

    #2

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import  express, { Express, Request, Response } from 'express';
 import { QuoteHandlerInjector } from '.././lib/handlers/quote/injector';
 import { QuoteHandler } from '.././lib/handlers/quote/quote';
 
+import { QuoteToRatioHandlerInjector } from '.././lib/handlers/quote-to-ratio/injector';
+import { QuoteToRatioHandler } from '.././lib/handlers/quote-to-ratio/quote-to-ratio';
+
 const port = 5050;
 
 const app: Express = express();
@@ -10,14 +13,26 @@ const app: Express = express();
 const quoteInjectorPromise = new QuoteHandlerInjector('quoteInjector').build()
 const quoteHandler = new QuoteHandler('quote', quoteInjectorPromise)
 
+const quoteToRatioInjectorPromise = new QuoteToRatioHandlerInjector('quoteToRatioInjector').build()
+const quoteToRatioHandler = new QuoteToRatioHandler('quote', quoteToRatioInjectorPromise)
 
-app.get('/', (request: Request, response: Response) => {
+app.get('/quote', (request: Request, response: Response) => {
   console.log(request.cookies);
   
   quoteHandler: quoteHandler.handler;
 
   response.type('text/plain');
-  response.send('Homepage');
+  response.send('/quote');
+})
+
+
+app.get('/quoteToRatio', (request: Request, response: Response) => {
+  console.log(request.cookies);
+  
+  quoteToRatioHandler: quoteToRatioHandler.handler;
+
+  response.type('text/plain');
+  response.send('/quoteToRatio');
 })
 
 app.listen(port, () => {
